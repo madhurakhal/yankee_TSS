11c7c446a4489647b5f02f4fef47d2ed9925c562
Shriharsh <Shriharsh@192.168.0.4>
2017-09-11 19:44:52 +0530

    adding content to timesheet branch

---
+++ yankee_TSS-ejb/src/java/jee17/logic/TimeSheetBusinessLogic.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jee17.logic;
+
+import jee17.logic.to.Contract;
+
+/**
+ *
+ * @author Shriharsh
+ */
+public interface TimeSheetBusinessLogic {
+ 
+    public String createTimeSheet(Contract contract);
+}
---
+++ yankee_TSS-ejb/src/java/jee17/logic/to/Contract.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jee17.logic.to;
+
+import java.time.LocalDate;
+import jee17.logic.ENUM.ContractStatusEnum;
+import jee17.logic.ENUM.TimesheetFrequencyEnum;
+
+/**
+ *
+ * @author Shriharsh
+ */
+public class Contract {
+    
+    private ContractStatusEnum status;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private TimesheetFrequencyEnum frequency;
+    private LocalDate terminationDate;
+    private double hoursPerWeek;
+    private int workingDaysPerWeek;
+    private int vacationDaysPerYear;
+
+    public ContractStatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(ContractStatusEnum status) {
+        this.status = status;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public TimesheetFrequencyEnum getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(TimesheetFrequencyEnum frequency) {
+        this.frequency = frequency;
+    }
+
+    public LocalDate getTerminationDate() {
+        return terminationDate;
+    }
+
+    public void setTerminationDate(LocalDate terminationDate) {
+        this.terminationDate = terminationDate;
+    }
+
+    public double getHoursPerWeek() {
+        return hoursPerWeek;
+    }
+
+    public void setHoursPerWeek(double hoursPerWeek) {
+        this.hoursPerWeek = hoursPerWeek;
+    }
+
+    public int getWorkingDaysPerWeek() {
+        return workingDaysPerWeek;
+    }
+
+    public void setWorkingDaysPerWeek(int workingDaysPerWeek) {
+        this.workingDaysPerWeek = workingDaysPerWeek;
+    }
+
+    public int getVacationDaysPerYear() {
+        return vacationDaysPerYear;
+    }
+
+    public void setVacationDaysPerYear(int vacationDaysPerYear) {
+        this.vacationDaysPerYear = vacationDaysPerYear;
+    }
+
+   
+    
+    
+    
+}
