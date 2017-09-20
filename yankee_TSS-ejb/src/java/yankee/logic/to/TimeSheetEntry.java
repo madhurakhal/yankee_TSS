9b8bc8a82f3ef8a60c98b111cd6c8855c185dc12
Shriharsh <Shriharsh@192.168.0.4>
2017-09-20 15:21:00 +0530

    initial commit

---
+++ yankee_TSS-ejb/src/java/yankee/logic/to/TimeSheetEntry.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yankee.logic.to;
+
+import java.sql.Time;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Shriharsh Ambhore (ashriharsh@uni-koblenz.de)
+ */
+public class TimeSheetEntry {
+    
+    private String description;
+    private Time startTime;
+    private Time endTime;
+    private LocalDate entryDate;
+    private Long timeSheetId;
+    private String dateString;
+
+    public String getDateString() {
+        return dateString;
+    }
+
+    public void setDateString(String dateString) {
+        this.dateString = dateString;
+    }
+
+    public Boolean getIsHoliday() {
+        return isHoliday;
+    }
+
+    public void setIsHoliday(Boolean isHoliday) {
+        this.isHoliday = isHoliday;
+    }
+    
+    private Boolean isHoliday;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Time getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+    public Time getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Time endTime) {
+        this.endTime = endTime;
+    }
+
+    public LocalDate getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(LocalDate entryDate) {
+        this.entryDate = entryDate;
+    }
+
+    public Long getTimeSheetId() {
+        return timeSheetId;
+    }
+
+    public void setTimeSheetId(Long timeSheetId) {
+        this.timeSheetId = timeSheetId;
+    }
+    
+    
+    
+    
+}
