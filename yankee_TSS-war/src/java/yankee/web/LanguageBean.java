c7edeab611a9ce72aea462590741409fa61fcd6b
Madhu Rakhal Magar <rakhal.madhu@gmail.com>
2017-09-20 11:28:19 +0200

    language change button implemented

--- yankee_TSS-war/src/java/yankee/web/LanguageBean.java
+++ yankee_TSS-war/src/java/yankee/web/LanguageBean.java
@@ -6,25 +6,38 @@
 package yankee.web;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+
 /**
  *
  * @author T540P
  */
 
- 
-@ManagedBean(name="language")
+@ManagedBean(name = "language")
 @SessionScoped
-public class LanguageBean implements Serializable{
-	
-	private static final long serialVersionUID = 1L;
-	
-	private Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+public class LanguageBean implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    private Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+    private final Map<String, String> availableLanguages;
+
+    public Map<String, String> getAvailableLanguages() {
+        return availableLanguages;
+    }
+    
+    public LanguageBean() {
+        availableLanguages = new HashMap<>();
+        availableLanguages.put("en", "English");
+        availableLanguages.put("de", "Deutsch");
+    }
+    
     public Locale getLocale() {
         return locale;
     }
