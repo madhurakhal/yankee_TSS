e9ad433a94e271916b466990aa2a6e0e413804f3
Sabin Bhattarai <sabin.bhattarai@hotmail.com>
2017-09-28 17:39:02 +0200

    Programattic time runs on settings set by adminstration. i.e. on and off reminder service.

--- yankee_TSS-war/src/java/yankee/services/ReminderService.java
+++ yankee_TSS-war/src/java/yankee/services/ReminderService.java
@@ -9,6 +9,8 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
+import yankee.logic.AdministrationBusinessLogic;
+import yankee.logic.impl.AdministrationBusinessLogicImpl;
 import yankee.logic.to.TimeSheet;
 import yankee.web.ReminderBean;
 
@@ -25,67 +27,27 @@ public class ReminderService {
     @EJB
     ReminderBean reminderBean;
 
+    @EJB
+    AdministrationBusinessLogic administrationBusinessLogic;
+
     /*
         @Schedules ({
             @Schedule(dayOfMonth="Last"),
             @Schedule(dayOfWeek="Fri", hour="23"), dayOfMonth = "-7"
         })
      */
-    // Scheduler ejb to do certain task based on param, - 7 means last 7 days of the month
     //@Schedule(hour = "06", minute = "59", second = "59", persistent = false)
     @Schedule(minute = "*/1", hour = "*", persistent = false)
     public void runTask() {
         System.out.println("This task is executed");
         programmaticTimer.cancelTimer("timerId");
-        List<TimeSheet> timeSheets = reminderBean.getTimeSheetsToSendReminder();
-        if (timeSheets!= null && timeSheets.size() > 0) {
-            System.out.println("called from reminder service = " + timeSheets);
-            reminderBean.sendReminderForTimeSheets(timeSheets);
-            programmaticTimer.createTimer("timerId", 1);
-        }
-    }
-
-    /*public void getRequiredData() {
-        try {
-            employees = employeeBusinessLogic.getEmployeeList();
-            System.out.println("list of employees  " + employees);
-        } catch (Exception e) {
-            System.out.println("Exception Occured from employee business logic!!");
-        }
-
-        if (employees != null) {
-            for (Employee e : employees) {
-                String contractId = e.getContract().getUuid();
-                TimesheetFrequencyEnum contractFrequency = e.getContract().getFrequency();
-                try {
-                    timeSheets = timeSheetBusinessLogic.getAllTimeSheetsForContract(contractId);
-                    System.out.println("list of timesheets  " + timeSheets);
-                } catch (NumberFormatException ne) {
-                    System.out.println("Exception Occured from timeSheetBusinessLogic!!");
-                }
-                if (timeSheets != null) {
-                    for (TimeSheet t : timeSheets) {
-                        System.out.println("Status =" + t.getStatus());
-                        timesheetstatus = t.getStatus();
-
-                        switch (timesheetstatus) {
-                            case IN_PROGRESS:
-                                // send email to employee
-                                sendReminderToEmployee(e);
-                                break;
-                            case SIGNED_BY_EMPLOYEE:
-                                // send email to supervisor and assistant
-                                sendReminderToSupervisorAssistant(contractId);
-                                break;
-
-                            case SIGNED_BY_SUPERVISOR:
-                                // send eamil to secretaries
-                                sendReminderToSecretaries(contractId);
-                                break;
-                        }
-                    }
-                }
+        if (administrationBusinessLogic.getAdminSettingsInfo().isReminderServiceOn()) {           
+            List<TimeSheet> timeSheets = reminderBean.getTimeSheetsToSendReminder();
+            if (timeSheets != null && timeSheets.size() > 0) {
+                System.out.println("called from reminder service = " + timeSheets);
+                reminderBean.sendReminderForTimeSheets(timeSheets);
+                programmaticTimer.createTimer("timerId", 1);
             }
         }
-    } */
+    }
 }
